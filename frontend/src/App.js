676e9e0727d08dfe5439360977440b23a15a16c6
RaphaelBecker <raphael.becker.private@outlook.de>
2023-04-26 16:54:26 +0200

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Navbar";
+import ContactForm from "./Components/Contact";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
