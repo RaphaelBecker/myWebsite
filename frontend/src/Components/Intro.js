6d38427ba7dc35437b361986d7833500430a6b90
RaphaelBecker <raphael.becker.private@outlook.de>
2023-05-04 22:05:55 +0200

    made image scale down if background gets too small

--- frontend/src/Components/Intro.js
+++ frontend/src/Components/Intro.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Col, Container, Row, Image, Button } from "react-bootstrap";
 import Header from "./Navbar";
 import IntroTextBlock from "./SmallComponents/IntroTextBlock";
 
 const Intro = ({ scrollToRef }) => {
   return (
-    <div className="h-screen bg-gradient-to-tr from-green-400 to-blue-600">
+    <div className="min-h-screen bg-gradient-to-tr from-green-400 to-blue-600">
       <Header scrollToRef={scrollToRef} />
       <div className="px-8 lg:py-0 xl:px-20 xl:pt-20 2xl:pt-32 2xl:px-52">
         <div className="flex flex-col lg:flex-row items-center">
           <div className="lg:w-1/2 flex items-center justify-center">
             <img
-              className="rounded-xl shadow-2xl m-8"
+              className="rounded-xl shadow-2xl m-8 max-h-96 object-contain"
               src="art_foto.png"
               alt="myKonterfei"
             />
